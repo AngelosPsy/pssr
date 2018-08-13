c5b1b877b940355a91e46d575016f2a30aef8e88
AngelosPsy <amkrypotos@gmail.com>
2018-08-13 14:14:22 +0200

    Now the app opens the browser by default.

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -1,11 +1,11 @@
 #' Shiny app for the \code{pssr} package
-#'
+#' @param browser Whether the app should use the browser or not -- default to \code{TRUE}
 #' @description Lanches a Shiny app for performing the core analyses included
 #' in \code{pssr}
 #' @details
 #' The function can be called without any arguments (i.e., pssr::shiny_app()).
 #'
 #' @export
-shiny_app <- function (){
-  shiny::runApp(system.file('app', package='pssr'))
+shiny_app <- function (browser = TRUE){
+  shiny::runApp(system.file('app', package='pssr'), launch.browser = browser)
 }
